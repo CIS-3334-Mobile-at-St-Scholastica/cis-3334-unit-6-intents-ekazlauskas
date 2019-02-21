3a51c42a08de89fba4b5f0b5dcfb050782bcfc21
ekazlauskas <patriotfever4@gmail.com>
2019-02-21 13:36:12 -0600

    Partially Completed Practice for Unit 6. Could not do 2nd and 3rd events.

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,7 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import java.util.GregorianCalendar;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1Web, but1Calendar, btn1Map;
+    Button btn2Web, btn2Calendar, btn2Map;
     TextView textViewStatus;
 
     @Override
@@ -36,9 +38,20 @@ public class MainActivity extends AppCompatActivity {
         btn1Web.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
-                textViewStatus.setText("Code should display website for performance 1");
+                textViewStatus.setText("Opening CSS Spotlight Information...");
+                openCSSSpotlight1();
+
+            }
+
+            public void openCSSSpotlight1(){
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=view%3Devent%26eventid%3D129826104");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
+
 
         /**
          *   Set up button click event listener for the celendar button for the first performance
@@ -48,13 +61,14 @@ public class MainActivity extends AppCompatActivity {
         but1Calendar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
-                textViewStatus.setText("Code should set a calendar event for performance 1");
+                textViewStatus.setText("Opening Calendar...");
                 Intent calIntent = new Intent(Intent.ACTION_INSERT);
                 calIntent.setType("vnd.android.cursor.item/event");
-                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
-                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
-                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
-                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.Events.TITLE, "\n" +
+                        "Twin Ports Choral Project-All Things New'");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+                GregorianCalendar begDate = new GregorianCalendar(2019, 1, 22, 19, 30);
+                GregorianCalendar endDate = new GregorianCalendar(2019, 1, 22, 21, 30);
                 calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
                         begDate.getTimeInMillis());
                 calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
@@ -72,8 +86,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textViewStatus.setText("Code should display map for performance 1");
+                Uri location = Uri.parse("https://www.google.com/maps?q=1200+Kenwood+Ave,+Duluth,+MN+55811");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startActivity(mapIntent);
             }
         });
+
 
 
     }
